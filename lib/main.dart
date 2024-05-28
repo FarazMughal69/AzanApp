796439e69a5a30a17b5eb58dd40d8ec775cd6aa9
Faraz Baig <Faraz.pu2020@gmail.com>
2024-05-28 19:16:13 +0500

    debug banner remove

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     mq = MediaQuery.of(context).size;
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Azan App',
       initialRoute: "/login",
       routes: {
